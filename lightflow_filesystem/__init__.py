5c4ade32bc264efc0ec054c1198d1cc835831d08
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2017-11-16 18:00:34 +1100

    Add task that removes files and directories

--- lightflow_filesystem/__init__.py
+++ lightflow_filesystem/__init__.py
@@ -8,6 +8,7 @@ from .notify_trigger_task import NotifyTriggerTask
 from .makedir_task import MakeDirTask
 from .copy_task import CopyTask
 from .move_task import MoveTask
+from .remove_task import RemoveTask
 from .chown_task import ChownTask
 from .chmod_task import ChmodTask
 from .glob_task import GlobTask
